55bf1a04df59fd584d681d4449be7f970042a91c
meroykiran <158093683+meroykiran@users.noreply.github.com>
2024-01-30 01:19:05 +0530

    Hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,48 @@
+// // #include<iostream>
+
+// // using namespace std;
+
+// // int main() {
+
+// //     cout<<"Hello world";
+
+// // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+// cout<<"Hello Kiran"<<endl;
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+string name;
+
+cout<< "Enter your Name:- ";
+cin>> name;
+cout<<"Whats up? "<<name<< endl;
+
+
+return 0;
+
+}
+
+// #include <iostream>
+// using namespace std;
+
+// int main (){
+
+//     int a;
+//     cout<<"How old are you? :-";
+//     cin>>a;
+//     cout<<"Your age is: - "<<a;
+
+
+//     return 0;
+// }
