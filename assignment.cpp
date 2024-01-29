7403362104e9ed47af93eb8bce0e0464e2962588
meroykiran <158093683+meroykiran@users.noreply.github.com>
2024-01-30 02:20:14 +0530

    Assignment

---
+++ assignment.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+string name;
+cout<<"What is your name?"<< endl;
+cin>>name;
+
+int age;
+cout<<"What is you age?"<< endl;
+cin>>age;
+
+char gender;
+cout<<"What is your gender? (M/F)"<< endl;
+cin>>gender;
+
+float weight;
+cout<<"What is your weight?"<< endl;
+cin>>weight;
+
+
+cout<<"Hello! " <<name<< endl;
+cout<<"Your age is :- " <<age<< endl;
+cout<<"Your gender is :-" <<gender<< endl;
+cout<<"Your weight is :- " <<weight<< endl;
+
+
+    return 0;
+}
